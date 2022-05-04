3d982892b9e7c24f719bab21306193da249c30b5
iammiori <mioriii@naver.com>
2022-05-04 23:50:32 +0900

    feat - 뷰컨에 collectionview 추가

--- JangBiBBal/MainViewController.swift
+++ JangBiBBal/MainViewController.swift
@@ -7,12 +7,16 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class MainViewController : UIViewController {
+    
+    let tagCV = TagCollectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setAttribute()
+        setLayout()
     }
 }
 
@@ -20,5 +24,13 @@ extension MainViewController {
     private func setAttribute() {
         self.title = NaviTitle.mainTitle
         view.backgroundColor = .white
+    }
+    private func setLayout() {
+        [tagCV].forEach { self.view.addSubview($0) }
+        tagCV.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.08)
+        }
     }
 }
