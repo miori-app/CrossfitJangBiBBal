705e5d0cc5d81f9753b4e20042c5e07026df6429
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-05-05 23:44:07 +0900

    feat - 디바이스 스크린 크기 얻어오는 constant 지정 (#2)

---
+++ JangBiBBal/Configuration/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  JangBiBBal
+//
+//  Created by miori Lee on 2022/05/04.
+//
+
+import UIKit
+
+//MARK: 디바이스의 스크린 크기
+struct ScreenConstant {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+}
