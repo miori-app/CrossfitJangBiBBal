8d3e7893073c01a90f3477a2624da57d2f08bfa9
iammiori <mioriii@naver.com>
2022-05-08 23:32:54 +0900

    feat - response model 구현

---
+++ JangBiBBal/Network/Entity/ShoppingResponse.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingResponse.swift
+//  JangBiBBal
+//
+//  Created by miori Lee on 2022/05/06.
+//
+
+import Foundation
+
+struct ShoppingResponse : Decodable {
+    let total : Int
+    let items : [ShoppingItems]
+}
+
+struct ShoppingItems : Decodable {
+    let title : String
+    let link : String
+    let image : String
+    let lprice : String
+    let brand : String
+}
