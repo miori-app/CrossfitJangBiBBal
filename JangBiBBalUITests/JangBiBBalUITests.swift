a0a989fba21d54a3728f636e869977030df3d22e
Mi Yeon Lee <immioriii@gmail.com>
2022-06-06 00:03:29 +0900

    UITest : 네비게이션 타이틀 테스트

--- JangBiBBalUITests/JangBiBBalUITests.swift
+++ JangBiBBalUITests/JangBiBBalUITests.swift
@@ -8,34 +8,28 @@
 import XCTest
 
 class JangBiBBalUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    var targetApp : XCUIApplication!
+    
+    
+    override func setUp() {
+        super.setUp()
+        
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        targetApp = XCUIApplication()
+        targetApp.launch()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        targetApp = nil
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testNavigationBarTitle() {
+        let existNaviationBar = targetApp.navigationBars["장비빨로 pr 가즈아"].exists
+        XCTAssertTrue(existNaviationBar)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
 }
