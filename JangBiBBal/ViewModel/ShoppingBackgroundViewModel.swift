6ddd349de1e91bc05c8ec3c473d686e032ff5ab8
Mi Yeon Lee <immioriii@gmail.com>
2022-06-08 23:36:15 +0900

    #21 : 백그라운드 뷰 라벨을 보이게 할지 말지를 결정할 observable 변수 선언

---
+++ JangBiBBal/ViewModel/ShoppingBackgroundViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingBackgroundViewModel.swift
+//  JangBiBBal
+//
+//  Created by miori Lee on 2022/06/07.
+//
+
+import Foundation
+
+class ShoppingBackgroundViewModel {
+    
+    var isHidden : Observable<Bool> = Observable(false)
+    
+}
